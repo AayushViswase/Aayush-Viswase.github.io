389878a0a1c2c54c6aef84935a59fba382b13edd
AayushViswase <aayushviswase10@gmail.com>
2023-07-23 22:12:50 +0530

    protfolio

---
+++ js/main.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .row').isotope({
+            filter: selector,
+            layoutMode: "fitRows"
+            
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .row .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+
+
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+    function clear(){
+        let name = document.getElementById("name");
+        name.innerText="";
+        let email = document.getElementById("email");
+        email.innerText="";
+        let message = document.getElementById("message");
+        message.innerText = "";
+    }
+
+    clear();
+
+});
+
+document.querySelector('.click1').addEventListener("click", () => {
+    document.querySelector("#navbarNav").classList.toggle("show")
+})
+
+document.querySelector('.click2').addEventListener("click", () => {
+    document.querySelector("#navbarNav").classList.toggle("show")
+})
+document.querySelector('.click3').addEventListener("click", () => {
+    document.querySelector("#navbarNav").classList.toggle("show")
+})
+document.querySelector('.click4').addEventListener("click", () => {
+    document.querySelector("#navbarNav").classList.toggle("show")
+})
+document.querySelector('.click5').addEventListener("click", () => {
+    document.querySelector("#navbarNav").classList.toggle("show")
+})
+
+// Smooth Scrolling Effect
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
